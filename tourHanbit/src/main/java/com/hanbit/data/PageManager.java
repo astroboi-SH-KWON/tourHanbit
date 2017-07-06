dbce882ab36e26512f381ade7da91761a35b5692
1027 <1027@hb4014>
2017-07-06 19:43:42 +0900

    재도전

--- tourHanbit/src/main/java/com/hanbit/data/PageManager.java
+++ tourHanbit/src/main/java/com/hanbit/data/PageManager.java
@@ -25,6 +25,6 @@ public class PageManager {
 	public void getMainImage(){
 		SqlSession session=	factory.openSession();
 		session.selectList("");
-		System.out.println("돌");
+		System.out.println("돌아이");
 	}
 }
