bbd30f1e5dcf0d03045aa69f55307936ebf670c2
1027 <1027@hb4014>
2017-07-17 20:25:59 +0900

    검색 페이지 진행중

--- tourHanbit/src/main/java/com/hanbit/controller/SearchController.java
+++ tourHanbit/src/main/java/com/hanbit/controller/SearchController.java
@@ -1,16 +1,42 @@
 package com.hanbit.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.hanbit.dao.PackageDao;
+
 @Controller
 public class SearchController {
 	
+	@Autowired
+	private PackageDao dao;
+		
+	public void setDao(PackageDao dao) {
+		this.dao = dao;
+	}
+
 	@RequestMapping("/srch.do")
 	public ModelAndView search(String srchOption,String srch,String startDay,String endDay, String amount){
 		ModelAndView mav= new ModelAndView();
+		String item=null;
+		if(srchOption.equals("0")||srchOption.equals("1")||srchOption.equals("2")){
+			if(srchOption.equals("1")||srchOption.equals("2")){
+				item=srchOption+"%";
+			}
+			mav.addObject("list", dao.serchResults(item,"%"+srch+"%",null,null,null));
+
+		}
 		
+		
+		
+		
+		
+		
+		
+		mav.addObject("viewPage", "srch.jsp");
+		mav.setViewName("/template");
 		return mav;
 	}
 }
--- tourHanbit/src/main/java/com/hanbit/data/PageManager.java
+++ tourHanbit/src/main/java/com/hanbit/data/PageManager.java
@@ -56,4 +56,25 @@ public class PageManager {
 		session1.close();
 		return list;
 	}
+	public static List<PackageVo> serchResults(String item, String srch, String startDay, String endDay,
+			String amount) {
+		// TODO Auto-generated method stub
+		SqlSession session1=	factory1.openSession();
+		HashMap<String, String> map= new HashMap<String, String>();
+		map.put("item", item);
+		map.put("srch", srch);
+		map.put("startDay", startDay);
+		map.put("endDay", endDay);
+		map.put("amount", amount);
+		List<PackageVo> list=	session1.selectList("page.serchResults",map);
+		System.out.println("pageManager"+list.size());
+		//System.out.println("srchOption : "+srchOption);
+		System.out.println("item : "+item);
+		System.out.println("srch : "+srch);
+		System.out.println("startDay : "+startDay);
+		System.out.println("endDay : "+endDay);
+		System.out.println("amount : "+amount);
+		session1.close();
+		return list;
+	}
 }
